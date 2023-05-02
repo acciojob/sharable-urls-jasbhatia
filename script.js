cf412887608643610d0cce9d882d28fc243d6801
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-02 16:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+let btn = document.getElementById("submit");
+console.log(btn);
+btn.addEventListener("click", () => {
+  // e.preventDefault();
+  let nameVal = document.getElementById("name").value;
+  let yearVal = document.getElementById("year").value;
+  str = "https://localhost:8080/";
+  if (nameVal && yearVal) {
+    str += `?name=${nameVal}&year=${yearVal}`;
+    document.getElementById("url").innerHTML = str;
+  } else if (nameVal) {
+    str += `?name=${nameVal}`;
+    document.getElementById("url").innerHTML = str;
+  } else if (yearVal) {
+    str += `?year=${yearVal}`;
+    document.getElementById("url").innerHTML = str;
+  }
+
+  // document.getElementById("url").innerHTML = yearVal;
+});
